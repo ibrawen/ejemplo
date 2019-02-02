faf56c5fb1d46d2438739bb8d554ed4999cce24c
programacion <programacion@fkinal.edu>
2019-02-02 11:50:26 -0600

    agregando linea de saludo

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ejemplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("bienvenido al curso de .NET");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
